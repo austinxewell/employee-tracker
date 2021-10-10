ef8a09c3d54bde1432cdb63b414e0acd66b9826b
AustinxEwell <austin.ewell86@gmail.com>
2021-10-10 08:40:05 -0600

    added initial prompt for navigation

---
+++ index.js
@@ -0,0 +1,30 @@
+const inquirer = require('inquirer');
+
+
+// start up prompt
+const int = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'routeOption',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments.',
+                'View all roles.',
+                'View all employees.',
+                '**View employees by manager.**',
+                '**View employees by department',
+                'Add a department.',
+                'Add a role.',
+                'Add an Employee.',
+                '**Delete a department**',
+                '**Delete a role.**',
+                '**Delete an employee**',
+                'Update an Employee Role',
+                '**Update employee managers.**'
+            ],
+        }
+    ])
+}
+
+int();
